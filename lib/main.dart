eebbee3b907ffa1099392de72a12a900428224fa
Chette <80092129+MrChette@users.noreply.github.com>
2023-02-23 09:15:48 +0100

    register()

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:frontspring/screens/login_screen.dart';
+import 'package:frontspring/screens/register_screen.dart';
+import 'package:frontspring/services/auth_service.dart';
+import 'package:frontspring/services/verify_service.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(const AppState());
+
+class AppState extends StatelessWidget {
+  const AppState({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => AuthService(),
+          lazy: false,
+        ),
+        ChangeNotifierProvider(
+          create: (_) => VerifyService(),
+          lazy: false,
+        ),
+
+        // ChangeNotifierProvider(create: (_) => AuthService()),
+      ],
+      child: MyApp(),
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.€%%
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        initialRoute: 'login',
+        routes: {
+          'login': (_) => const LoginScreen(),
+          'register': (_) => const RegisterScreen(),
+        });
+  }
+}
